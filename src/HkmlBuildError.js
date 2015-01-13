0fe6cd95be75a269b33d050c70b245c3b4d2abeb
Shirobok Pavel <deeptrader@yandex.ru>
2015-01-13 19:39:09 +0800

    HkmlBuildError completed. Code is copy-pasted from http://stackoverflow.com/a/8460753

--- src/HkmlBuildError.js
+++ src/HkmlBuildError.js
@@ -1,2 +1,8 @@
-//TODO make error
-var HkmlBuildError = function(){}
+// code from http://stackoverflow.com/a/8460753
+function HkmlBuildError(message) {
+    this.constructor.prototype.__proto__ = Error.prototype // Make this an instanceof Error.
+    Error.call(this) // Does not seem necessary. Perhaps remove this line?
+    Error.captureStackTrace(this, this.constructor) // Creates the this.stack getter
+    this.name = this.constructor.name; // Used to cause messages like "UserError: message" instead of the default "Error: message"
+    this.message = message; // Used to set the message
+}
