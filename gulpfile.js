3f9417c49a63ab0bc717b0972239a86c8de52fc7
Shirobok Pavel <deeptrader@yandex.ru>
2015-01-20 14:21:17 +0800

    New order of files in gulpfile. All keyboard button in Keyboard.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,20 @@ var watch = require('gulp-watch');
 
 var version = require('./package.json').version;
 
+var SOURCES = [
+    './src/HkmlBuildError.js',
+    './src/HkmlCompiler.js',
+    './src/HkmlKeyboardController.js',
+    './src/HkmlVM.js',
+    './src/Keyboard.js',
+    './src/KeysNode.js',
+    './src/utils.js',
+    './src/export.js'
+];
+
 gulp.task('build', function() {
 
-    return gulp.src('src/**/*.js').
+    return gulp.src(SOURCES).
         pipe(uglify(
             'hkml-' + version +'.js',
             {
@@ -25,7 +36,7 @@ gulp.task('build', function() {
 });
 
 gulp.task('build-min', function() {
-    return gulp.src('src/**/*.js').
+    return gulp.src(SOURCES).
         pipe(uglify(
                 'hkml-' + version +'.min.js',
                 {
@@ -36,6 +47,6 @@ gulp.task('build-min', function() {
         pipe(gulp.dest('build/'));
 });
 
-gulp.task('example-js', function(){
+gulp.task('example-js', ['build', 'build-min'], function(){
     return gulp.src('build/**/*.js').pipe(gulp.dest('example/js'));
 });
